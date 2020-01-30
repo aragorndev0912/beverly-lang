4300fe6080ed41ebbe71d3a662f6b1e5d2d90e9d
aragorndev0912 <aragorn.dev0912@gmail.com>
2020-01-30 23:46:00 +0100

    add clear command

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include "src/repl/repl.h"
+#include <stdlib.h>
 
 int main(void) {
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+    system("cls");
+#elif defined(__linux__)
+    system("clear");
+#endif
+
     Repl repl = new_repl(">> ");
     start_repl(&repl);
     free_repl(&repl);
